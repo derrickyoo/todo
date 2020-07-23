23ad4a702a9e303c692dd86e107dbcc200e7694e
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-07-23 09:27:49 -0700

    Update task fields and test factories

--- backend/todo/todo/tasks/models.py
+++ backend/todo/todo/tasks/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
 
 
-class Task(models.Model):
+class Task(TimeStampedModel):
     title = models.CharField(max_length=200)
+    slug = AutoSlugField(
+        unique=True, always_update=False, populate_from="title"
+    )
     body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title[:15]
--- backend/todo/todo/tasks/tests/factories.py
+++ backend/todo/todo/tasks/tests/factories.py
@@ -1,7 +1,37 @@
+from typing import Any, Sequence
+
+from django.contrib.auth.models import User
+
 import factory
 import factory.fuzzy
+from factory import post_generation
 
 from ..models import Task
+
+
+class UserFactory(factory.DjangoModelFactory):
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+
+    @post_generation
+    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
+        password = (
+            extracted
+            if extracted
+            else factory.Faker(
+                "password",
+                length=42,
+                special_chars=True,
+                digits=True,
+                upper_case=True,
+                lower_case=True,
+            ).generate(extra_kwargs={})
+        )
+        self.set_password(password)
+
+    class Meta:
+        model = User
+        django_get_or_create = ["username"]
 
 
 class TaskFactory(factory.DjangoModelFactory):
@@ -9,6 +39,7 @@ class TaskFactory(factory.DjangoModelFactory):
     body = factory.Faker(
         "paragraph", nb_sentences=3, variable_nb_sentences=True
     )
+    author = factory.SubFactory(UserFactory)
 
     class Meta:
         model = Task
