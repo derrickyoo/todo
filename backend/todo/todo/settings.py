253544502c2c657f120ccfe64cbce123f3e52734
Derrick Yoo <4998684+derrickyoo@users.noreply.github.com>
2020-07-23 13:42:24 -0700

    Implement token registration for API

--- backend/todo/todo/settings.py
+++ backend/todo/todo/settings.py
@@ -37,10 +37,17 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # Third party
+    "corsheaders",
     "django_extensions",
     "rest_framework",
-    "corsheaders",
+    "rest_framework.authtoken",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     # Local
     "todo.tasks.apps.TasksConfig",
 ]
@@ -131,10 +138,18 @@ SHELL_PLUS = "ipython"
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:4200",
     "http://localhost:8000",
 )
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+SITE_ID = 1
